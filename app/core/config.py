6bbe30b0f8ec2817a0a7802fcb1bf6e3a69acb3a
Yaroslav <yroslavkumpan@gmail.com>
2026-03-18 21:41:45 +0300

    add logger, price,time file

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,11 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str = "postgresql+asyncpg://user:password@db:5432/crypto"
-    REDIS_URL: str = "redis://localhost:6379/0"
+    DB_URL: str
+    REDIS_URL: str
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
